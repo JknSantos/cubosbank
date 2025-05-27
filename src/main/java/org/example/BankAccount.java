2388585b8eda42501d8fedaf05845bdc316812af
Jackson Felipe do Nascimento Santos <jacksonfelipe.contact@gmail.com>
2025-05-26 23:10:48 -0300

    criação da classe abstrata BankAccount

---
+++ src/main/java/org/example/BankAccount.java
@@ -0,0 +1,51 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class BankAccount {
+    private String customerName;
+    private String billingAddress;
+    private double balance;
+    private List<String> transactions;
+
+    public BankAccount(String customerName, String billingAddress, double initialBalance) {
+        this.customerName = customerName;
+        this.balance = initialBalance;
+        this.transactions = new ArrayList<>();
+        this.transactions.add("Saldo inicial: " + initialBalance);
+    }
+
+    public void deposit(double amount) {
+        if(amount > 0) {
+            balance += amount;
+            transactions.add("Depósito: " + amount);
+        }
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public String getBillingAddress() {
+        return billingAddress;
+    }
+
+    public void setBillingAddress(String billingAddress) {
+        this.billingAddress = billingAddress;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public List<String> getTransactions() {
+        return transactions;
+    }
+
+}
+
