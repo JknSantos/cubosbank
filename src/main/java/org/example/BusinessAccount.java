5dbfc7b5b709f3419b0f18caee7c9cbb0bc8f7fd
Jackson Felipe do Nascimento Santos <jacksonfelipe.contact@gmail.com>
2025-05-26 23:46:13 -0300

    Criação da BusinnesAccount

---
+++ src/main/java/org/example/BusinessAccount.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class BusinessAccount extends BankAccount {
+
+    private String cnpj;
+
+    public BusinessAccount(String customerName, String billingAddress, double initialBalance, String cnpj) {
+        super(customerName, billingAddress, initialBalance);
+        this.cnpj = cnpj;
+    }
+
+
+}
