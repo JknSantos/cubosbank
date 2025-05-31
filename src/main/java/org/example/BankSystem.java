0922a49b7751d4a9d725877bf6c5234525a14df9
Jackson Felipe  - Desenvolvedor de software <71566554+JknSantos@users.noreply.github.com>
2025-05-31 18:08:30 -0300

    Implementação de novos métodos e ajuste dos existentes e definição de um cliente de teste (#5)
    * Implementação dos métodos CreateAccount e ActionsAccountAfterLogin
    
    * Ajuste no método deleteAccount

--- src/main/java/org/example/BankSystem.java
+++ src/main/java/org/example/BankSystem.java
@@ -2,6 +2,7 @@ package org.example;
 
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Scanner;
 
 public class BankSystem {
@@ -20,7 +21,7 @@ public class BankSystem {
         System.out.println("Digite o nome do cliente: ");
         String name = scanner.nextLine();
 
-        System.out.println("Digite o endereço de cobbrança: ");
+        System.out.println("Digite o endereço de cobrança: ");
         String address = scanner.nextLine();
 
         System.out.println("Digite uma senha: ");
@@ -43,21 +44,106 @@ public class BankSystem {
         } else if (accountType == 2) {
             System.out.println("Digite o CNPJ: ");
             String cnpj = scanner.nextLine();
-            PersonalAccount personalAccount = new PersonalAccount(name, address, balance, password, cnpj);
-            accountsDatabase.put(cnpj, personalAccount);
-            System.out.println("Conta pessoal criada com sucesso!");
+            BusinessAccount businessAccount = new BusinessAccount(name, address, balance, password, cnpj);
+            accountsDatabase.put(cnpj, businessAccount);
+            System.out.println("Conta juridica criada com sucesso!");
         } else {
             System.out.println("Operação inválida!");
         }
     }
 
-    public void login(String indetifier, String password) {
-        if (accountsDatabase.containsKey(indetifier)) {
-            BankAccount bankAccount = accountsDatabase.get(indetifier);
+    public void deleteAccount() {
+        System.out.println("Digite o CPF: ");
+        String cpf = scanner.nextLine();
+
+        if (accountsDatabase.containsKey(cpf)) {
+            BankAccount bankAccount = accountsDatabase.get(cpf);
+            int logginAttempt = 3;
+            System.out.println("Digite a senha: ");
+            do {
+                String password = scanner.nextLine();
+                if (!bankAccount.verifyPassword(password)) {
+                    System.out.println("senha invalida. Tente novamente!");
+                    logginAttempt--;
+                } else {
+                    accountsDatabase.remove(cpf);
+                    System.out.println("Conta bancaria removida com sucesso!");
+                    break;
+                }
+            } while (logginAttempt != 0);
+        } else {
+            System.out.println("Conta não encontrada!");
+        }
+    }
+
+    public void actionsAccountAfterLogin(String indetifier, String password) {
+        BankAccount bankAccount = accountsDatabase.get(indetifier);
+        int option;
+        do {
+            System.out.println("Escolha uma opção: ");
+            System.out.println("1 - Saque");
+            System.out.println("2 - Depositar");
+            System.out.println("3 - Transferir");
+            System.out.println("4 - Extrato");
+            System.out.println("0 - Sair");
+
+            option = scanner.nextInt();
+            scanner.nextLine();
+
+            switch (option) {
+                case 1:
+                    System.out.println("Digite a quantidade: ");
+                    int quantidadeSaque = scanner.nextInt();
+                    scanner.nextLine();
+                    bankAccount.withdraw(quantidadeSaque);
+                    break;
+                case 2:
+                    System.out.println("Digite a quantidade: ");
+                    int quantidadeDeposito = scanner.nextInt();
+                    scanner.nextLine();
+                    bankAccount.deposit(quantidadeDeposito);
+                    break;
+                case 3:
+                    //TODO a ser implementado
+                    System.out.println("Digite a quantidade: ");
+                    int quantidadeTransferencia = scanner.nextInt();
+                    scanner.nextLine();
+                    break;
+                case 4:
+                    List<String> transactions = bankAccount.getTransactions();
+                    System.out.println(transactions);
+
+                    System.out.println("1. Imprimir extrato");
+                    System.out.println("0. voltar");
+                    option = scanner.nextInt();
+                    scanner.nextLine();
+                    if (option == 1) {
+                        bankAccount.printStatement();
+                    } else {
+                        System.out.println("Voltando ao menu");
+                    }
+                case 0:
+                    System.out.println("Até a proxima " + bankAccount.getCustomerName());
+            }
+        } while (option != 0);
+    }
+
+    public void login() {
+
+        System.out.println("Login...");
+        System.out.println("Digite seu CPF: ");
+        String cpf = scanner.nextLine();
+        System.out.println("Digite sua senha: ");
+        String password = scanner.nextLine();
+
+        if (accountsDatabase.containsKey(cpf)) {
+            BankAccount bankAccount = accountsDatabase.get(cpf);
             boolean passwordMatch = bankAccount.verifyPassword(password);
 
             if (passwordMatch) {
                 System.out.println("Usuario logado com sucesso. Bem vindo(a) " + bankAccount.getCustomerName());
+                System.out.println("Saldo atual: " + bankAccount.getBalance());
+                actionsAccountAfterLogin(cpf, password);
             }
         } else {
             System.out.println("Conta não encontrada");
@@ -67,7 +153,8 @@ public class BankSystem {
     public static void main(String[] args) {
         HashMap<String, BankAccount> accounts = new HashMap<>();
         Scanner scanner = new Scanner(System.in);
-
+        PersonalAccount personalAccountTeste = new PersonalAccount("Jackson Felipe", "Rua Peru", 6000, "123", "123");
+        accounts.put("123", personalAccountTeste);
         BankSystem bankSystem = new BankSystem(accounts, scanner);
 
         int option;
@@ -86,6 +173,15 @@ public class BankSystem {
                 case 0:
                     System.out.println("Saindo...");
                     break;
+                case 1:
+                    bankSystem.login();
+                    break;
+                case 2:
+                    bankSystem.createAccount();
+                    break;
+                case 3:
+                    System.out.println("Deletar conta...");
+                    bankSystem.deleteAccount();
                 default:
                     System.out.println("Operação invalida. Tente novamente.");
             }
